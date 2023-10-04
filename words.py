0937820930f76a126a5bf2d7be27248da5e39520
wisteen <wisdomisaac168@gmail.com>
2023-10-04 14:56:44 +0100

    Create words.py
    Contain list of animals used in the game

---
+++ words.py
@@ -0,0 +1,12 @@
+word_list = [
+    "Lion", "Tiger", "Elephant", "Giraffe", "Zebra",
+    "Kangaroo", "Koala", "Panda", "Hippopotamus", "Rhino",
+    "Cheetah", "Leopard", "Gorilla", "Orangutan", "Chimpanzee",
+    "Penguin", "Polar Bear", "Grizzly Bear", "Puma", "Coyote",
+    "Wolf", "Fox", "Red Panda", "Sloth", "Koala",
+    "Camel", "Gazelle", "Kangaroo", "Koala", "Platypus",
+    "Dolphin", "Whale", "Shark", "Octopus", "Seahorse",
+    "Turtle", "Crocodile", "Alligator", "Giraffe", "Llama",
+    "Zebra", "Giraffe", "Gorilla", "Panda", "Chimpanzee",
+    "Lioness", "Cheetah", "Leopard", "Tiger", "Hippopotamus"
+]
